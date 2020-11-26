47352bf50d82d41d12080b12e99c258507dd95ba
Kateitsme <katyaefremova02@yandex.ru>
2020-11-27 02:31:08 +0300

    hw 5

--- src/test/java/hw/TestHw.java
+++ src/test/java/hw/TestHw.java
@@ -1,16 +1,23 @@
 package hw;
 
+import com.sun.org.glassfish.gmbal.Description;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.Keys.ENTER;
+
 public class TestHw {
-    public static YandexPage page;
     public static WebDriver driver;
 
     @BeforeClass
@@ -18,7 +25,6 @@ public class TestHw {
         WebDriverManager.chromedriver().setup();
         //создание экземпляра драйвера
         driver = new ChromeDriver();
-        page = new YandexPage(driver);
         driver.manage().window().maximize();
         //задержка на выполнение теста = 10 сек.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -27,28 +33,62 @@ public class TestHw {
     }
 
     @Test
-    public void covidTest() throws InterruptedException {
-
-        //page.clickCovid();
-        //по клику не работают следующие методы, не поняла как исправить
-        driver.navigate().to("https://yandex.ru/covid19/stat?utm_source=main_notif&geoId=11119");
-        page.findClick();
-        page.sendMoscow();
-        page.goodClick();
-        page.badClick();
-        page.waitTime(5);
+    @Description("Тест на вход в лк")
+    public void loginTest() throws InterruptedException {
+        WebElement login =  driver.findElement(By.xpath("//a[text()='Войти']"));
+        Assert.assertTrue(login.isDisplayed());
+        login.click();
+        new Actions(driver)
+                .moveToElement(driver.findElement(By.xpath("//div/input[@type='email']")))
+                .sendKeys(ConfProperties.getProperty("login"))
+                .sendKeys(ENTER)
+                .build()
+                .perform();
+        Thread.sleep(1000);
+        new Actions(driver)
+                .moveToElement(driver.findElement(By.xpath("//div/input[@type='password']")))
+                .sendKeys(ConfProperties.getProperty("password"))
+                .sendKeys(ENTER)
+                .build()
+                .perform();
     }
 
     @Test
-    public void weatherTest() throws InterruptedException {
-        //page.weatherClick();
-        //по клику не работают следующие методы, не поняла как исправить
-        driver.navigate().to("https://yandex.ru/pogoda/");
-        page.inputCity();
-        page.inputCityClick();
-        page.mscClick();
-        page.forMonthClick();
-        page.waitTime(5);
+    @Description("Тест на изменение имени пользователя")
+    public void changeUserInfo() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        loginTest();
+        driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        new Actions(driver)
+                .moveToElement(driver.findElement(By.xpath("//img[@class='gb_Ha gbii']")))
+                .click()
+                .build()
+                .perform();
+        WebElement acc = driver.findElement(By.xpath("//a[text()='Управление аккаунтом Google']"));
+        Assert.assertTrue(acc.isDisplayed());
+        js.executeScript("arguments[0].click();", acc);
+        new WebDriverWait(driver,5).until(ExpectedConditions.numberOfWindowsToBe(2));
+        ArrayList<String> tabs = new ArrayList(driver.getWindowHandles());
+        tabs.forEach(tab->{
+            System.out.println(tab);
+            driver.switchTo().window(tab);
+        });
+        //так в тесте перекидывает в проверку конфиценциальности
+        //WebElement info = driver.findElement(By.xpath("//a[@data-nav-type='1']/div[text()='Личная информация']"));
+        //js.executeScript("arguments[0].click();", info);
+        driver.navigate().to("https://myaccount.google.com/personal-info?pli=1");
+        js.executeScript("arguments[0].click();",driver
+                .findElement(By.xpath("(//a[@class='VZLjze Wvetm I6g62c N5YmOc kJXJmd'])[1]")));
+        new Actions(driver)
+                .moveToElement(driver.findElement(By.xpath(("(//input)[3]"))))
+                .doubleClick()
+                .sendKeys(Keys.DELETE)
+                .sendKeys(ConfProperties.getProperty("name"))
+                .build()
+                .perform();
+        js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//span[text()='Сохранить']")));
+        driver.manage().addCookie(new Cookie("info", "november27"));
+        driver.manage().deleteCookieNamed("info");
     }
 
     @AfterClass
